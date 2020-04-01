32b5afc190ee2094c9a2846735bb46b5f6e502e8
Pranav Shikarpur <contact@snpranav.com>
2020-03-31 20:57:29 -0400

    Added multiple language feature

--- static/json.js
+++ static/json.js
@@ -1,50 +1,73 @@
 $(document).ready(function() {
+    let language;
+    let queryString = window.location.search;
+    let urlParams = new URLSearchParams(queryString);
+    let lang = urlParams.get('lang');
 
+    new Promise((resolve, reject) => {
+        return fetch(`./static/${lang}.json`).then(response => {
+            if(response.status==200) {
+                language = lang;
+            } else {
+                language = "kannada";
+            }
+            resolve(response.status);
+        })
+    }).then(() => {
     // Pulling dynamic app-content
-    let submitButtonText;
-    let radioButtonCharacter = ``;
-    $.getJSON(`http://localhost:8080/static/${language}.json`, function(data) {
-        $.each(data["app-content"], function(key, val) {
-            $.each(val, function(className, classValues) {
-                $.each(classValues, function(textKey, textValue) {
-                    if(className===".option") {
-                        $.each(textValue, function(optionKey, optionValue) {
-                            if(optionKey < $(`${key} #formpage-${textKey} ${className}`).length) {
-                                try {
-                                    optionValue = radioButtonCharacter + optionValue;
-                                    $(`${key} #formpage-${textKey} ${className}`)[optionKey].append(optionValue);
-                                } catch(err) {
-                                    console.warn(err);
+    console.log(language);
+        let submitButtonText;
+        let radioButtonCharacter = ``;
+        if(language==undefined) {
+            console.log(language);
+            language = "kannada";
+        }
+
+        $.getJSON(`./static/${language}.json`, function(data) {
+            $.each(data["app-content"], function(key, val) {
+                $.each(val, function(className, classValues) {
+                    $.each(classValues, function(textKey, textValue) {
+                        if(className===".option") {
+                            $.each(textValue, function(optionKey, optionValue) {
+                                if(optionKey < $(`${key} #formpage-${textKey} ${className}`).length) {
+                                    try {
+                                        optionValue = radioButtonCharacter + optionValue;
+                                        $(`${key} #formpage-${textKey} ${className}`)[optionKey].append(optionValue);
+                                    } catch(err) {
+                                        console.warn(err);
+                                    }
                                 }
-                            }
-                        });
-                    } else if(key==="#intro-text") {
-                            if(textKey < $(`${key} ${className}`).length) {
+                            });
+                        } else if(key==="#intro-text") {
+                                if(textKey < $(`${key} ${className}`).length) {
+                                    $(`${key} ${className}`)[textKey].innerHTML += textValue;
+                                } else {
+                                    if(className!=".title") {
+                                        $(`${key} ${className}`).parent().eq(textKey-1).clone().appendTo($(`${key} ${className}`).parent().eq(textKey-1));
+                                        $(`${key} ${className}`)[textKey].innerHTML = textValue;
+                                    }
+                                }
+                        } else {
+                            try {
                                 $(`${key} ${className}`)[textKey].innerHTML += textValue;
-                            } else {
-                                $(`${key} ${className}`).parent().eq(textKey-1).clone().appendTo($(`${key} ${className}`).parent().eq(textKey-1));
-                                $(`${key} ${className}`)[textKey].innerHTML = textValue;
+                            } catch (err) {
+                                console.warn(err);
                             }
-                    } else {
-                        try {
-                            $(`${key} ${className}`)[textKey].innerHTML += textValue;
-                        } catch (err) {
-                            console.warn(err);
                         }
-                    }
+                    });
                 });
             });
+
+
+            // Pulling Static Elements
+            let staticElements = data["static"];
+            try {
+                document.title = staticElements["title"] + " | " + document.title;
+                $("#restart-btn").innerHTML = staticElements["restart-btn"];
+                submitButtonText = staticElements["submit-btn"];
+            } catch (err) {
+                console.warn(err);
+            }
         });
-
-
-        // Pulling Static Elements
-        let staticElements = data["static"];
-        try {
-            document.title = staticElements["title"] + " | " + document.title;
-            $("#restart-btn").innerHTML = staticElements["restart-btn"];
-            submitButtonText = staticElements["submit-btn"];
-        } catch (err) {
-            console.warn(err);
-        }
     });
 });
