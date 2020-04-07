04c3c3f62f1266766b2bec27e67904b611f8048c
Pranav Shikarpur <contact@snpranav.com>
2020-04-06 21:49:04 -0400

    Added dynamic language select feature

---
+++ langselect.js
@@ -0,0 +1,68 @@
+// try {
+//     import('dotenv').config();
+// } catch(err) {
+//     console.warn(err);
+// }
+
+$(document).ready(() => {
+    let regionName;
+    try {
+        fetch(`https://ipapi.co/json/?key=${IPAPI_API_KEY}`)
+        .then((response) => {
+            return response.json();
+        }).then((data) => {
+            regionName = data["region"];
+        }).then(() => {
+            writeLanguages();
+        }).catch(error => {
+            writeLanguages();
+            console.warn(error);
+        });
+    } catch(err) {
+        writeLanguages();
+        console.warn(error);
+    }
+
+    function insertLanguageInHTML(key, val) {
+        $(`#lang-select`).eq(0)
+        .prepend(`
+            <label class=" btn btn-default btn-lg lang-select-btn">
+                <input type="radio" onclick="languageClick(${key})" />
+                &ensp; ${val["text"]} &rarr;<br />
+            </label>
+
+            <br /><br />
+
+        `);
+    }
+
+    function writeLanguages() {
+        fetch('./language-select.json')
+            .then(response => {
+                return response.json();
+            }).then((data) => {
+                let arrayOfLanguages = Object.keys(data);
+                try {
+                    // If regionName is undefined setting timeout
+                    if(regionName == undefined) {
+                        setTimeout(() => {
+                        },1000);
+                    } else {
+                        arrayOfLanguages.forEach((lang) => {
+                            if(data[lang]["region"].includes(regionName)) {
+                                arrayOfLanguages.splice(arrayOfLanguages.indexOf(lang), 1);
+                                arrayOfLanguages.unshift(lang);
+                            }
+                        });
+                    }
+                } catch (err) {
+                    console.warn(err);
+                } finally {
+                    arrayOfLanguages.reverse();
+                    arrayOfLanguages.forEach((lang) => {
+                        insertLanguageInHTML(lang, data[lang]);
+                    });
+                }
+            })
+    }
+});
