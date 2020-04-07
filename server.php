eb2d51f4a3466a1513c94fbbaf06639031ca93f7
thesuhailcompany <42859854+thesuhailcompany@users.noreply.github.com>
2020-04-07 21:55:58 +0530

    Update server.php

--- server.php
+++ server.php
@@ -1,18 +1,4 @@
 <?php
-$ip = $_SERVER['REMOTE_ADDR']; 
-echo $ip;
-
-if($ip == "10.122.0.6"){
-    echo "COVIDSELFCHECK-DO-01";
-}
-
-if($ip == "10.122.0.7"){
-    echo "COVIDSELFCHECK-DO-02";
-}
-
-if($ip == "10.122.0.8"){
-    echo "COVIDSELFCHECK-DO-03";
-}
-
+echo "COVIDSELFCHECK-DO-01";
 
 ?>
