7e20b9fbb264dc59588160f72f75c191681e0f39
thesuhailcompany <42859854+thesuhailcompany@users.noreply.github.com>
2020-04-07 21:49:16 +0530

    Update server.php

--- server.php
+++ server.php
@@ -1,14 +1,17 @@
 <?php
 $ip = $_SERVER['REMOTE_ADDR']; 
-switch ($ip) {
-    case "10.122.0.6":
-        echo "COVIDSELFCHECK-DO-01";
-        break;
-    case "10.122.0.7":
-        echo "COVIDSELFCHECK-DO-02";
-        break;
-    case "10.122.0.8":
-        echo "COVIDSELFCHECK-DO-03";
-        break;
+
+if($ip == "10.122.0.6"){
+    echo "COVIDSELFCHECK-DO-01";
 }
+
+if($ip == "10.122.0.7"){
+    echo "COVIDSELFCHECK-DO-02";
+}
+
+if($ip == "10.122.0.8"){
+    echo "COVIDSELFCHECK-DO-03";
+}
+
+
 ?>
